dc0ebd076ffc485c99cc6e00015fd95495a695e9
Mateusz Jamrocha <matper92@gmail.com>
2025-02-18 20:24:21 +0100

    added split_data method

--- kod.py
+++ kod.py
@@ -15,9 +15,10 @@ class Dataset:
             self.headers.append(self.data_list[0])
             self.data_list.pop(0)
 
+
     def get_headers(self):
         if self.headers == []:
-            print('No headers in this data set')
+            print('Nie ma etykiet w tym datasecie')
         else:
             for line in self.headers:
                 print(line.split(','))
@@ -31,19 +32,59 @@ class Dataset:
             for line in self.sliced_data_list:
                 print(line)
 
+
     def split_data(self,trening=0, test=0, walidacja=0):
+        """
+        Metoda rozdzielająca zbiór na trzy podzbiory. Dane w żadnym z podzbiorów nie dublują się.
+        """
         self.zbior_treningowy =[]
         self.zbior_testowy = []
         self.zbior_walidacyjny = []
+        self.wykorzystane_indeksy =[]
 
-        for n in range(round(len(self.data_list)*trening/100)):
-            self.zbior_treningowy.append(self.data_list[random.randint(0,len(self.data_list))])
+
+        if trening + test + walidacja > 100:
+            print('suma parametrów nie może być większa niz 100')
+        else:
+            # zbiór treningowy
+            self.counter = 0
+            while self.counter < round(len(self.data_list)*trening/100):
+                self.random_index = random.randint(0,len(self.data_list)-1)
+                if self.random_index not in self.wykorzystane_indeksy:
+                    self.zbior_treningowy.append(self.data_list[self.random_index])
+                    self.wykorzystane_indeksy.append(self.random_index)
+                    self.counter += 1
+
+            # zbiór testowy
+            self.counter = 0
+            while self.counter < round(len(self.data_list)*test/100):
+                self.random_index = random.randint(0, len(self.data_list)-1)
+                if self.random_index not in self.wykorzystane_indeksy:
+                    print(self.random_index)
+                    self.zbior_testowy.append(self.data_list[self.random_index])
+                    self.wykorzystane_indeksy.append(self.random_index)
+                    self.counter += 1
+
+            # zbiór walidacyjny
+            self.counter = 0
+            while self.counter < round(len(self.data_list)*walidacja/100):
+                self.random_index = random.randint(0, len(self.data_list)-1)
+                if self.random_index not in self.wykorzystane_indeksy:
+                    self.zbior_walidacyjny.append(self.data_list[self.random_index])
+                    self.wykorzystane_indeksy.append(self.random_index)
+                    self.counter += 1
 
 
 
 d1 = Dataset()
-d1.load_data('car.data', True)
+d1.load_data('car.data', False)
 d1.get_headers()
 # d1.get_data(10,20)
-d1.split_data(15)
-print(len(d1.zbior_treningowy))
+d1.split_data(15,30,60)
+print('zbior treningowy',len(d1.zbior_treningowy))
+print(d1.wykorzystane_indeksy)
+print('zbior testowy', len(d1.zbior_testowy))
+print(d1.wykorzystane_indeksy)
+print('zbior walidacyjny', len(d1.zbior_walidacyjny))
+print(d1.wykorzystane_indeksy)
+print(len(d1.data_list))
